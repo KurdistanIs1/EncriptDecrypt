0a5489b6192be1082d5cb3570908d8230151cf72
KurdistanIs1 <98112941+KurdistanIs1@users.noreply.github.com>
2022-02-16 19:41:16 +0100

    Create start.py

---
+++ start.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# by anonym-r.ga
+
+import os
+import sys
+import fileinput
+
+N = '\033[0m'
+D = '\033[90m'
+W = '\033[1;37m'
+B = '\033[1;34m'
+R = '\033[1;31m'
+G = '\033[1;32m'
+Y = '\033[1;33m'
+C = '\033[1;36m'
+
+ask = G + '[' + W + '?' + G + '] '
+sukses = G + '[' + W + '√' + G + '] '
+eror = R + '[' + W + '!' + R + ']'
+
+banner = """{}    _                              ___ \n   /_\  _ _  ___ _ _ _  _ _ __ ___| _ \ \n  / _ \| ' \/ _ \ ' \ || | '  \___|   / \n /_/ \_\_||_\___/_||_\_, |_|_|_|  |_|_\ \n                     |__/{}""".format(R,Y)
+
+banner2 = """
+   {}[{}2{}]{} Decrypt      {}[{}1{}]{} Encript
+""".format(G,W,G,W,G,W,G,W)
+
+print banner
+print banner2
+
+def dekrip():
+   try:
+       sc = raw_input(ask + W + "Script " + G + "> " + W)
+       f = open(sc,'r')
+       filedata = f.read()
+       f.close()
+
+       newdata = filedata.replace("eval","echo")
+
+       out = raw_input(ask + W + "Output" + G + " > " + W)
+       f = open(out,'w')
+       f.write(newdata)
+       f.close()
+
+       os.system("touch tes.sh")
+       os.system("bash " + out + " > tes.sh")
+       os.remove(out)
+       os.system("mv -f tes.sh " + out)
+       print (sukses + "Done..")
+
+   except KeyboardInterrupt:
+       print (eror + " Stopped!")
+   except IOError:
+       print (eror + " File Not Found!")
+
+def enkrip():
+   try:
+       script = raw_input(ask + W + "Script " + G + "> " + W)
+       output = raw_input(ask + W + "Output " + G + "> " + W)
+       os.system("bash-obfuscate " + script + " -o " + output )
+       print (sukses + "Done..")
+   except KeyboardInterrupt:
+       print (eror + " Stopped!")
+   except IOError:
+       print (eror + " File Not Found!")
+
+
+takok = raw_input(G + "[" + R + "*" + G + "]"  + Y + " >> ")
+
+if takok == "1" or takok == "01":
+   enkrip()
+elif takok == "2" or takok == "02":
+   dekrip()
+else:
+   print (eror + " Wrong input")
